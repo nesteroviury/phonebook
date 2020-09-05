fab3230cd7b6d1c5f8ec62bc3bc2df368209f32f
yan <nesteroviury@yandex.ru>
2020-09-05 13:34:17 +0400

    entities added

---
+++ domain/src/main/java/ru/dev/interview/project/phonebook/domain/entities/AddressType.java
@@ -0,0 +1,34 @@
+package ru.dev.interview.project.phonebook.domain.entities;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Getter
+@NoArgsConstructor
+@Setter
+@ToString
+public class AddressType {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String value;
+    @OneToMany(mappedBy = "type")
+    private Set<Address> addresses = new HashSet<>();
+
+    public AddressType(String value) {
+        this.value = value;
+    }
+}
---
+++ domain/src/main/java/ru/dev/interview/project/phonebook/domain/entities/Contact.java
@@ -0,0 +1,38 @@
+package ru.dev.interview.project.phonebook.domain.entities;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Getter
+@NoArgsConstructor
+@Setter
+@ToString
+public class Contact {
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue
+    private Long id;
+    private String value;
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "type_id")
+    private ContactType type;
+    @OneToOne(mappedBy = "contact")
+    private Person person;
+
+    public Contact(String value, ContactType type) {
+        this.value = value;
+        this.type = type;
+    }
+}
